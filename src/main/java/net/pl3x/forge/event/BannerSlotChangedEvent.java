a6b11741533be05e93b7578ed75f21168fa3e0b8
BillyGalbreath <Blake.Galbreath@GMail.com>
2017-11-23 22:05:35 -0600

    Add EntityPlayer to BannerSlotChangedEvent

--- src/main/java/net/pl3x/forge/event/BannerSlotChangedEvent.java
+++ src/main/java/net/pl3x/forge/event/BannerSlotChangedEvent.java
@@ -1,5 +1,6 @@
 package net.pl3x.forge.event;
 
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.ContainerPlayer;
 import net.minecraft.inventory.Slot;
 import net.minecraft.item.ItemStack;
@@ -8,10 +9,12 @@ import net.minecraftforge.fml.common.eventhandler.Event;
 public class BannerSlotChangedEvent extends Event {
     private final ContainerPlayer container;
     private final Slot slot;
+    private final EntityPlayer player;
 
-    public BannerSlotChangedEvent(ContainerPlayer container, Slot slot) {
+    public BannerSlotChangedEvent(ContainerPlayer container, Slot slot, EntityPlayer player) {
         this.container = container;
         this.slot = slot;
+        this.player = player;
     }
 
     public ContainerPlayer getContainer() {
@@ -20,6 +23,10 @@ public class BannerSlotChangedEvent extends Event {
 
     public Slot getSlot() {
         return slot;
+    }
+
+    public EntityPlayer getPlayer() {
+        return player;
     }
 
     public ItemStack getItemStack() {
